1bb9272c81c7a7f7aa516ce911187e42ccd75ccd
jbferguson <jbferguson85@outlook.com>
2021-02-07 17:32:43 -0600

    Improve add command.

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -2,27 +2,27 @@ const fs = require('fs');
 const chalk = require('chalk');
 const fileName = 'notes.json';
 
-const getNotes = () =>  'Your notes...';
+const getNotes = () => 'Your notes...';
 
 const addNote = (title, body) => {
     const notes = loadNotes();
-    const duplicateNotes = notes.filter((note) =>  note.title === title);
+    const duplicateNote = notes.find((note) => note.title === title);
 
-    if (duplicateNotes.length === 0) {
+    if (duplicateNote) {
+        console.log(chalk.red.inverse('Note with that title already exists.'));
+    } else {
         notes.push({
             title,
             body
         });
         saveNotes(notes);
         console.log(chalk.green.inverse('Note saved.'));
-    } else {
-        console.log(chalk.red.inverse('Note with that title already exists.'));
     }
 };
 
 const removeNote = (title) => {
     const notes = loadNotes();
-    const notesToKeep = notes.filter((note) =>  note.title !== title);
+    const notesToKeep = notes.filter((note) => note.title !== title);
 
     if (notes.length > notesToKeep.length) {
         console.log(chalk.green.inverse('Note removed.'));
@@ -35,7 +35,7 @@ const removeNote = (title) => {
 const listNotes = () => {
     const notes = loadNotes();
     console.log('Made it in');
-    if(notes.length === 0) {
+    if (notes.length === 0) {
         console.log(chalk.red.inverse('You have no notes currently.'));
     } else {
         console.log(chalk.blue.inverse('Your notes:'));
