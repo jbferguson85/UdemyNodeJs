f18a98daa6f71d540cdc477cd826fe3aadf44cff
jbferguson <jbferguson85@outlook.com>
2021-02-07 19:54:19 -0600

    Get started with weatherstack api.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 second timer');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=654b378456810bff189ab0ce02cb9af6&query=37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0)
-
-console.log('Stopping');
+request({url}, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+});
