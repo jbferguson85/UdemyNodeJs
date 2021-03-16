942d560b96524434009fb8551475eac54f34364c
jbferguson <jbferguson85@outlook.com>
2021-03-16 17:38:03 -0500

    Complete course up until callbacks challenge.

---
+++ playground/4-callbacks.js
@@ -0,0 +1,32 @@
+// setTimeout(() => {
+//     console.log('2 seconds are up')
+// }, 2000);
+
+// const names = ['JB', 'Joe', 'Lady'];
+// const shortNames = names.filter((name) => {
+//     return name.length <= 2;
+// });
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         };
+//          callback(data)
+//     }, 2000);
+// }
+
+// geocode('Grand Island', (data) => {
+//     console.log(data);
+// });
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 2000);
+};
+
+add(1, 4, (sum) => {
+    console.log(sum);
+});
